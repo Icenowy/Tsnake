6a88bd2f85ff75986b8da2190a9675d002b05ae6
Icenowy Zheng <icenowy@outlook.com>
2015-12-13 17:07:36 +0800

    split fix

--- src/split.cpp
+++ src/split.cpp
@@ -13,11 +13,11 @@ void init_split ()
 	WINDOW *pg_with_box = subwin (stdscr, pgrows+2, pglines+2, 0,0);
 	playground = subwin (pg_with_box, pgrows, pglines, 1, 1);
 	info = subwin (stdscr, INFO_ROWS, pglines, pgrows + 2, 0);
-	WINDOW *goods_with_line = subwin (stdscr, pgrows + INFO_ROWS + 2, GOODS_LINES + 1, 0, pglines+2);
-	goods = subwin (goods_with_line, pgrows + INFO_ROWS + 2, GOODS_LINES, 0, 1);
+	WINDOW *goods_line = subwin (stdscr, pgrows + INFO_ROWS + 2, 1, 0, pglines+2);
+	goods = subwin (stdscr, pgrows + INFO_ROWS + 2, GOODS_LINES, 0, pglines+3);
 
 	wborder (pg_with_box, 0, 0, 0, 0, 0, 0, 0, 0);
-	mvwvline (goods_with_line, 0, 0, 0, pgrows + INFO_ROWS + 2);
+	mvwvline (goods_line, 0, 0, 0, pgrows + INFO_ROWS + 2);
 
 	wininfo = new cur_strwin (info, INFO_ROWS);
 
